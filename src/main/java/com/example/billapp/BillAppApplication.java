30aa590001a4e4909594c22838314f2743357645
Szymon Rusinek <Szymon@MacBook-Pro-SzymonR.local>
2023-07-15 13:49:28 +0200

    Inicjalny commit

---
+++ src/main/java/com/example/billapp/BillAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.billapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/billapp/BillAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
